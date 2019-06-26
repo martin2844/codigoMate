743c531045e1a841918c54fa9ce046aa6e49122b
Martin Chammah <martin2844@gmail.com>
2019-06-26 17:00:25 -0300

    test contact

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -18,7 +18,7 @@ const Contacto = () => {
       
 
 <h1>Envíame un mensaje</h1>
-<form name="contact" method="POST" data-netlify="true">
+<form name="contact1" method="POST" data-netlify="true">
   <p>
     <label >Your Name: <input type="text" name="name" /></label>   
   </p>
